fcd936d4ed725151b11094d3e05e09273eda2bfb
Vadim Tkachenko <vt@seriousinsights.com>
2021-12-10 20:01:56 -0700

    Moved versions from settings.gradle to build.gradle (#3)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,1 @@
-pluginManagement {
-    plugins {
-        id("com.github.spotbugs") version "4.0.5"
-        id("net.ltgt.errorprone") version "1.1.1"
-        id("org.sonarqube") version "3.2.0"
-    }
-}
-
 rootProject.name = "automation-hat-driver"
