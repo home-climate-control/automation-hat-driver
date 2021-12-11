2595489b87e1e99c780261ad7f2715fa1f42d0a7
Vadim Tkachenko <vt@seriousinsights.com>
2021-12-10 18:41:59 -0700

    Renamed files, updated syntax from Groovy to Kotlin (#3)

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+    id("jacoco")
+    id("net.ltgt.errorprone")
+    id("org.sonarqube")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+
+group = "com.homeclimatecontrol"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+
+    implementation("org.apache.logging.log4j:log4j-api:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
+
+    implementation("com.pi4j:pi4j-core:1.4")
+
+    testImplementation("org.mockito:mockito-core:4.1.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.21.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+
+    errorprone("com.google.errorprone:error_prone_core:2.10.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "home-climate-control_automation-hat-driver")
+        property("sonar.organization", "home-climate-control")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.6"
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        html.isEnabled = true
+    }
+}
